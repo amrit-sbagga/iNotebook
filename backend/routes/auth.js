bd76eeb5c744c18329f56ba78b647d538617cb9b
Amrit Singh <amrit.sbagga@gmail.com>
2021-09-04 20:51:33 +0530

    Storing Data into the db Using Mongoose Model

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-    obj = {
-        "name" : "Amrit"
-    }
-    res.json(obj)
+//create user using POST : /api/auth/
+router.post('/', (req, res) => {
+    console.log("body = ", req.body);
+    const user = User(req.body);
+    user.save();
+    res.send("user created successfully..!!");
 });
 
 module.exports = router;
