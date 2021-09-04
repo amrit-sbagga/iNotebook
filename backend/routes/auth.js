31ea31926d0ad8a33845df787b88dff89837c2fb
Amrit Singh <amrit.sbagga@gmail.com>
2021-09-05 01:45:58 +0530

    Added bcryptjs for pwd hashing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 //create user using POST : /api/auth/
 router.post('/createuser', [
@@ -16,9 +17,12 @@ router.post('/createuser', [
             console.log(errors.array());
             return res.status(400).json({errors : errors.array() });
         }
-        console.log("body = ", req.body);
+        //console.log("body = ", req.body);
         //const user = User(req.body);
         //user.save();
+
+        const salt = await bcrypt.genSalt(10);
+        let secPass = await bcrypt.hash(req.body.password, salt);
         
         //check whether user email already exists
         let user = await User.findOne({email : req.body.email});
@@ -31,7 +35,7 @@ router.post('/createuser', [
         user = await User.create({
             name : req.body.name,
             email : req.body.email,
-            password: req.body.password
+            password: secPass
         });
         
         // .then(user => res.json(user))
