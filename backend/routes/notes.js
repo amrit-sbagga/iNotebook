f961cd3d55e09f87874867226770de497f8b2aa5
Amrit Singh <amrit.sbagga@gmail.com>
2021-09-05 21:19:02 +0530

    Added get all notes route

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Notes = require('../models/Notes');
+const fetchuser = require('../middleware/fetchuser');
 
-router.get('/', (req, res) => {
-    obj = {
-        "name" : "Amrit"
-    }
-    res.json(obj)
+// ROUTE1 - Get all user notes using GET "/app/notes/fetchallnotes" [Login required]
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    const notes = await Notes.find({user : req.user.id});
+    console.log("notes = ", notes);
+    res.json({notes})
 });
 
 module.exports = router;
