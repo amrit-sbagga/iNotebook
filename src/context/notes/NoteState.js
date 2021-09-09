b2bdbfc19bc323a15be6d45c976a860c381e8fe8
Amrit Singh <amrit.sbagga@gmail.com>
2021-09-09 18:15:28 +0530

    Fetching Notes from Context

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,9 +2,20 @@ import NoteContext from './noteContext';
 import { useState } from 'react';
 
 const NoteState = (props) => {
+
+    const notesInitial = [
+        {
+            "title":"note1"
+        },
+        {
+            "title":"note2"
+        }
+   ];
+
+    const [notes, setNotes] = useState(notesInitial);
     
     return (
-        <NoteContext.Provider>
+        <NoteContext.Provider value={{notes, setNotes}}>
             { props.children }
         </NoteContext.Provider>
     )
