719cbc0760419bc383f85182d5abab2093ab7908
Amrit Singh <amrit.sbagga@gmail.com>
2021-09-11 00:11:14 +0530

    cors support added

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ const connectToMongo = require('./db');
 const express = require('express');
 
 connectToMongo();
-
+var cors = require('./cors');
 const app = express();
 const port = 5000;
 
 //middleware
+app.use(cors())
 app.use(express.json());
 
 //Available routes
